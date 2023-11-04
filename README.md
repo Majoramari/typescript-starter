21c699b774aaa3fe2cbe3a0f2f9f6e4343d4aaac
MajorAmari <major.amari@gmail.com>
2023-11-04 08:29:12 +0200

    version 0.0.5

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # typescript-starter
+
 A starter project for nodejs to help speed up the work...
+
+### commands:
+
+```
+pnpm install
+pnpm dev
+pnpm start
+```
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "module-alias/register";
 import "dotenv/config";
 
-import { helloWorld } from "@utils";
+import { helloWorld } from "@utils/helloWorld";
 
 const main = async () => {
   await helloWorld(process.env.ENV_TEST!);
