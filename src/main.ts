f56e5e76d5fd55175ae61c89af77ebbf4593e5cd
MajorAmari <major.amari@gmail.com>
2023-12-24 19:47:34 +0200

    Refactor main.ts and tsconfig.json

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-import "module-alias/register";
-import "dotenv/config";
-
 import { helloWorld } from "@utils/helloWorld";
 
-const main = async () => {
-  await helloWorld(process.env.ENV_TEST!);
-};
-
-main();
+await helloWorld(process.env.ENV_TEST!);
